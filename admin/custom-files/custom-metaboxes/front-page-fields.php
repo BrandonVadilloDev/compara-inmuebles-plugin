3fc98a5e0dfa7c42dc5f8f7bb0b1d489c7cb0dd4
Brandon Vadillo <brandon.vadillo@devitm.com>
2023-02-22 17:03:37 -0600

    Creacion de custom fields para slider de front page
    front-page-fields.php
     - Agrega el grupo repetible para crear slides en la front page
    class-compara_inmuebles_plugin.php
     - Se agrego el archivo de front-page-fields.php
    cmb2_functions.php
     - Se quito para que se mostraran.

---
+++ admin/custom-files/custom-metaboxes/front-page-fields.php
@@ -0,0 +1,108 @@
+<?php
+/**
+ * Estos son los campos personalizados para el custom post type de inmuebles
+ * 
+ * @link https://github.com/CMB2/CMB2
+ * @since v1.0.0
+ */
+
+if (!function_exists('register_properties_metaboxes_front_page')){
+  add_action( 'cmb2_init', 'register_properties_metaboxes_front_page' );
+  /**
+   * Hook in and add a demo metabox. Can only happen on the 'cmb2_admin_init' or 'cmb2_init' hook.
+   */
+  function register_properties_metaboxes_front_page() {
+    $prefix = 'front_page_';
+    $id_home = get_option('page_on_front');
+    /**
+     * Sample metabox to demonstrate each field type included
+     */
+    
+    $property_metabox = new_cmb2_box( array(
+      'id'            => $prefix.'metabox',
+      'title'         => esc_html__( 'Campos pagina inicio', 'cmb2' ),
+      'object_types'  => array( 'page' ), // Post type
+      'show_in_rest' => WP_REST_Server::ALLMETHODS, // WP_REST_Server::READABLE|WP_REST_Server::EDITABLE, // Determines which HTTP methods the box is visible in.
+      'show_on' => array(
+        'id' => array($id_home),
+      ),
+    ) );
+    $group_slider = $property_metabox->add_field( array(
+      'id'          => $prefix.'grupo_slider1',
+      'type'        => 'group',
+      'description' => __( 'Grupo de datos del slide', 'cmb2' ),
+      'repeatable'  => true, // use false if you want non-repeatable group
+      'options'     => array(
+        'group_title'       => __( 'Datos del slide {#}', 'cmb2' ), // since version 1.1.4, {#} gets replaced by row number
+        'add_button'        => __( 'Añadir otra slide', 'cmb2' ),
+        'remove_button'     => __( 'Remover slide', 'cmb2' ),
+        'sortable'          => true,
+        'closed'         => true, // true to have the groups closed by default
+        // 'remove_confirm' => esc_html__( 'Are you sure you want to remove?', 'cmb2' ), // Performs confirmation before removing group.
+      ),
+    ) );
+
+    $property_metabox->add_group_field( $group_slider, array(
+      'name' => 'Titulo',
+      'id'   => 'title',
+      'type' => 'text',
+      // 'repeatable' => true, // Repeatable fields are supported w/in repeatable groups (for most types)
+    ) );
+
+    $property_metabox->add_group_field( $group_slider, array(
+      'name' => 'Texto',
+      'description' => 'Texto para slide',
+      'id'   => 'description',
+      'type' => 'textarea_small',
+    ) );
+
+    $property_metabox->add_group_field( $group_slider, array(
+      'name' => 'Imagen slide',
+      'id'   => 'image',
+      'type' => 'file',
+    ) );
+
+    $property_metabox->add_group_field( $group_slider, array(
+      'name' => 'Texto boton 1',
+      'id'   => 'text_btn_1',
+      'type' => 'text',
+    ) );
+    
+    $all_pages = get_pages();
+    $options_select = array();
+    foreach ($all_pages as $page){
+      $options_select[get_permalink($page->ID)] = $page->post_title;
+    }
+
+    $property_metabox->add_group_field( $group_slider, array(
+      'name'             => 'Pagina a redireccionar boton 1',
+      'desc'             => 'Select an option',
+      'id'               => 'selec_btn_1',
+      'type'             => 'select',
+      'show_option_none' => true,
+      'options'          => $options_select,
+    ) );
+
+    $property_metabox->add_group_field( $group_slider, array(
+      'name' => 'Texto boton 2',
+      'id'   => 'text_btn_2',
+      'type' => 'text',
+    ) );
+
+    $property_metabox->add_group_field( $group_slider, array(
+      'name'             => 'Pagina a redireccionar boton 2',
+      'desc'             => 'Select an option',
+      'id'               => 'selec_btn_2',
+      'type'             => 'select',
+      'show_option_none' => true,
+      'options'          => $options_select,
+    ) );
+
+    $property_metabox->add_group_field($group_slider ,array(
+      'name' => 'Video youtube',
+      'desc' => 'Link de un video de youtube',
+      'id'   => 'video',
+      'type' => 'oembed',
+    ) );
+  }
+}
--- includes/class-compara_inmuebles_plugin.php
+++ includes/class-compara_inmuebles_plugin.php
@@ -132,6 +132,7 @@ class Compara_inmuebles_plugin {
 		  */
 		 require_once plugin_dir_path( dirname( __FILE__ ) ) . 'includes/cmb2_functions.php';
 		 require_once plugin_dir_path( dirname( __FILE__ ) ) . 'admin/custom-files/custom-metaboxes/inmuebles-fields.php';
+		 require_once plugin_dir_path( dirname( __FILE__ ) ) . 'admin/custom-files/custom-metaboxes/front-page-fields.php';
 
 		/**
 		 * 
