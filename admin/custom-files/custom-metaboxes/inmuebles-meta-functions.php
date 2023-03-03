b32020ee6b52f52e47080b8b035dd7582552877f
Marco Aspeitia <maspeitia.31@gmail.com>
2023-03-03 16:35:21 -0600

    implementing google ads metabox

--- admin/custom-files/custom-metaboxes/inmuebles-meta-functions.php
+++ admin/custom-files/custom-metaboxes/inmuebles-meta-functions.php
@@ -16,16 +16,20 @@ function set_post_views($post_id) {
     }
 }
 
-function register_inmuebles_new_meta(){
+function register_inmuebles_new_meta() {
+
   register_rest_field( 'inmuebles', 'post_views_count', array(
     'get_callback' => function ( $data ) {
       return get_post_meta( $data['id'], 'post_views_count', true );
-    }, ));
+    },
+  ));
 
-    register_rest_field( 'inmuebles', 'inmueble_direccion', array(
-      'get_callback' => function ( $data ) {
-        return get_post_meta( $data['id'], 'inmueble_direccion', true );
-      }, ));
+  register_rest_field( 'inmuebles', 'inmueble_direccion', array(
+    'get_callback' => function ( $data ) {
+      return get_post_meta( $data['id'], 'inmueble_direccion', true );
+    },
+  ));
+
 }
 add_action( 'rest_api_init', 'register_inmuebles_new_meta' );
 
@@ -43,23 +47,35 @@ function guardar_dir_inmueble($post_id, $post, $update){
     }
   }
 }
-
 add_action( 'save_post', 'guardar_dir_inmueble', 20, 3);
 
-function get_feature_media_url(){
-  register_rest_field( array('inmuebles'), 'featured_media_url', array(
-    'get_callback' => 'ci_get_featured_image',
-    'update_callback' => null,
-    'schema' => null,
-  ) );
+if( ! class_exists( 'get_feature_media_url' ) ) {
+
+  function get_feature_media_url() {
+
+    register_rest_field(
+      array('inmuebles'),
+      'featured_media_url',
+      array(
+        'get_callback' => 'ci_get_featured_image',
+        'update_callback' => null,
+        'schema' => null,
+      )
+    );
+  
+  }
+
 }
+
 
 add_action( 'rest_api_init', 'get_feature_media_url' );
 
-function ci_get_featured_image($object, $field_name, $request){
-  if($object['featured_media']){
+function ci_get_featured_image( $object, $field_name, $request ) {
+
+  if( $object['featured_media'] ) {
     $media = wp_get_attachment_image_src( $object['featured_media'], 'large', false);
     return $media[0];
   }
   return false;
+
 }
